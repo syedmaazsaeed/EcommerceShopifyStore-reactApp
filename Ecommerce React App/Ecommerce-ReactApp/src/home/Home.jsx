5fbeae48744c594b3de159443ece95120b2f711d
Syed Maaz Saeed <syedmaazsaeed@gmail.com>
2024-06-21 18:15:52 +0500

    Update Home.jsx
    Adding comments for better Understanding

--- Ecommerce React App/Ecommerce-ReactApp/src/home/Home.jsx
+++ Ecommerce React App/Ecommerce-ReactApp/src/home/Home.jsx
@@ -1,29 +1,37 @@
-import React from 'react';
-import Banner from './Banner';
-import HomeCategory from './HomeCategory';
-import CategoryShowCase from './CategoryShowCase';
-import ProductShowCase from './ProductShowCase';
-import Register from './Register';
-import Location from './Location';
-import AboutUs from './AboutUs';
-import AppSection from './AppSection';
-import Sponsor from './Sponsor';
+import React from 'react'; // Importing React library
+import Banner from './Banner'; // Importing Banner component
+import HomeCategory from './HomeCategory'; // Importing HomeCategory component
+import CategoryShowCase from './CategoryShowCase'; // Importing CategoryShowCase component
+import ProductShowCase from './ProductShowCase'; // Importing ProductShowCase component
+import Register from './Register'; // Importing Register component
+import Location from './Location'; // Importing Location component
+import AboutUs from './AboutUs'; // Importing AboutUs component
+import AppSection from './AppSection'; // Importing AppSection component
+import Sponsor from './Sponsor'; // Importing Sponsor component
 
 const Home = () => {
   return (
     <div>
-     <Banner/>
-     <HomeCategory/>
-     <CategoryShowCase/>
-     <ProductShowCase/>
-     <Register/>
-     <Location/>
-     <AboutUs/>
-     <AppSection/>
-     <Sponsor/>
+      {/* Display the main banner of the home page */}
+      <Banner />
+      {/* Showcase the main categories of the shop */}
+      <HomeCategory />
+      {/* Showcase specific categories with more details */}
+      <CategoryShowCase />
+      {/* Showcase highlighted products */}
+      <ProductShowCase />
+      {/* Section for user registration */}
+      <Register />
+      {/* Display the store's location */}
+      <Location />
+      {/* Provide information about the store */}
+      <AboutUs />
+      {/* Section to promote the store's app */}
+      <AppSection />
+      {/* Display sponsors of the store */}
+      <Sponsor />
     </div>
   );
 }
 
-export default Home;
-
+export default Home; // Exporting Home component
